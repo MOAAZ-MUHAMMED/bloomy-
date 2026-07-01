219e1d1dda5ab97ccb30017dc005129d8a95fce4
MOAAZ-MUHAMMED <moaazmuhamed2009@gmail.com>
2026-07-01 17:54:18 +0300

    feat: initialize Capacitor and generate native Android project files

---
+++ bloomly-kids/android/app/src/main/java/com/bloomly/kids/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bloomly.kids;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ bloomly-kids/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bloomly.kids',
+  appName: 'Bloomly Kids',
+  webDir: 'dist'
+};
+
+export default config;
