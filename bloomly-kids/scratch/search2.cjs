e0989eeca9fce129e37c1320eb03a6a58afc9343
MOAAZ-MUHAMMED <moaazmuhamed2009@gmail.com>
2026-07-21 01:03:17 +0300

    Fix routing for dailyHabits game

---
+++ bloomly-kids/scratch/search2.cjs
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const lines = fs.readFileSync('c:/Users/omar/Desktop/Huda-Nour-Site/bloomly-kids/src/components/GameZone.tsx', 'utf8').split('\n');
+lines.forEach((line, i) => {
+  if (line.includes('forcedGameId') || line.includes('function GameZone')) {
+    console.log(`${i+1}: ${line}`);
+  }
+});
