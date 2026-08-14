8a244b6f9a7a863dae473545dd0c0d0008cd1373
MOAAZ-MUHAMMED <moaazmuhamed2009@gmail.com>
2026-08-15 00:04:24 +0300

    Fix: restore DRACOLoader in Model3D to decode Draco-compressed 3D models and resolve white screen crashes on modals

--- bloomly-kids/src/components/Model3D.tsx
+++ bloomly-kids/src/components/Model3D.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 // @ts-ignore
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+// @ts-ignore
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 
 interface Model3DProps {
   src: string;
@@ -79,6 +81,10 @@ export default function Model3D({
     let animationFrameId: number;
 
     const loader = new GLTFLoader();
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.7/');
+    loader.setDRACOLoader(dracoLoader);
+
     loader.load(
       src,
       (gltf: any) => {
